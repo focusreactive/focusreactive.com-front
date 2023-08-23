210edcb4f1836c08cf166b9cea628616cdefd8fe
Anton Meleshkin <anton@focusreactive.com>
2023-08-23 12:29:29 +0400

    Add sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -16,66 +16,67 @@ const client = ClientObj.createClient({
 const builder = imageUrlBuilder(client);
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.on("eleventy.before", async () => {
-		await esbuild.build({
-			entryPoints: ["src/js/app.js"],
-			bundle: true,
-			sourcemap: true,
-			outfile: "_site/assets/js/app.js",
-		});
-	});
+  eleventyConfig.on("eleventy.before", async () => {
+    await esbuild.build({
+      entryPoints: ["src/js/app.js"],
+      bundle: true,
+      sourcemap: true,
+      outfile: "_site/assets/js/app.js",
+    });
+  });
 
-	eleventyConfig.setBrowserSyncConfig({
-		files: "./_site/assets/css/**/*.css",
-	});
+  eleventyConfig.setBrowserSyncConfig({
+    files: "./_site/assets/css/**/*.css",
+  });
 
-	eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
-		name: "serverless", // The serverless function name from your permalink object
-		redirects: false,
-		copy: ["node_modules"],
-	});
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: "serverless", // The serverless function name from your permalink object
+    redirects: false,
+    copy: ["node_modules"],
+  });
 
-	eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
-		name: "serverlessLandings", // The serverless function name from your permalink object
-		redirects: false,
-		copy: ["node_modules"],
-	});
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: "serverlessLandings", // The serverless function name from your permalink object
+    redirects: false,
+    copy: ["node_modules"],
+  });
 
-	eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
 
-	eleventyConfig.addWatchTarget("src/js");
-	eleventyConfig.addWatchTarget("src/sass");
+  eleventyConfig.addWatchTarget("src/js");
+  eleventyConfig.addWatchTarget("src/sass");
 
-	let nunjucksEnvironment = new Nunjucks.Environment(
-		new Nunjucks.FileSystemLoader("src/_includes"),
-		{
-			autoescape: false,
-		}
-	);
+  let nunjucksEnvironment = new Nunjucks.Environment(
+    new Nunjucks.FileSystemLoader("src/_includes"),
+    {
+      autoescape: false,
+    }
+  );
 
-	nunjucksEnvironment.addFilter("imgURL", function (value) {
-		return builder.image(value).auto("format").url();
-	});
+  nunjucksEnvironment.addFilter("imgURL", function (value) {
+    return builder.image(value).auto("format").url();
+  });
 
-	nunjucksEnvironment.addFilter("blockContent", function (value) {
-		return blocksToHtml({
-			blocks: value,
-		});
-	});
+  nunjucksEnvironment.addFilter("blockContent", function (value) {
+    return blocksToHtml({
+      blocks: value,
+    });
+  });
 
-	eleventyConfig.setLibrary("njk", nunjucksEnvironment);
+  eleventyConfig.setLibrary("njk", nunjucksEnvironment);
 
-	return {
-		addPassthroughFileCopy: true,
-		markdownTemplateEngine: "njk",
-		HTMLTemplateEngine: "njk",
-		dataTemplateEngine: "njk",
-		templateFormats: ["html", "njk", "md"],
-		dir: {
-			input: "src",
-			output: "_site",
-			includes: "_includes",
-			layouts: "_layouts",
-		},
-	};
+  return {
+    addPassthroughFileCopy: true,
+    markdownTemplateEngine: "njk",
+    HTMLTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    templateFormats: ["html", "njk", "md"],
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts",
+    },
+  };
 };
