bfaf64f063b3797623aca193e499c42da4c807db
lero62 <weblero2020@gmail.com>
2023-08-28 15:48:17 +0300

    fix widget github

---
+++ src/assets/js/github-repo-buttons.js
@@ -0,0 +1,323 @@
+! function(t) {
+  function e(r) {
+    if (n[r]) return n[r].exports;
+    var o = n[r] = {
+      i: r,
+      l: !1,
+      exports: {}
+    };
+    return t[r].call(o.exports, o, o.exports, e), o.l = !0, o.exports;
+  }
+  var n = {};
+  e.m = t, e.c = n, e.d = function(t, n, r) {
+    e.o(t, n) || Object.defineProperty(t, n, {
+      configurable: !1,
+      enumerable: !0,
+      get: r
+    });
+  }, e.n = function(t) {
+    var n = t && t.__esModule ? function() {
+      return t.default;
+    } : function() {
+      return t;
+    };
+    return e.d(n, 'a', n), n;
+  }, e.o = function(t, e) {
+    return Object.prototype.hasOwnProperty.call(t, e);
+  }, e.p = '', e(e.s = 0);
+}([function(t, e, n) {
+  var r, o, i, a;
+  if (n(1), i = {
+    stargazers: n(6),
+    watchers: n(7),
+    forks: n(8),
+    generic: n(9)
+  }, 'undefined' == typeof fetch || null === fetch) throw new Error('the fetch api is required!');
+  o = {}, window.ghButtonInstall = function(t) {
+    var e, n, r, a, u, s, c, p;
+    a = t.getAttribute('repo'), u = t.hasAttribute('insecure-http') ? 'http' : 'https';
+    var f;
+    if (f = function() {
+      if (t.hasAttribute('stars')) return ['stargazers', 'Star'];
+      if (t.hasAttribute('watchers')) return ['watchers', 'Watch'];
+      if (t.hasAttribute('forks')) return ['forks', 'Fork'];
+      throw new Error('github-repo-button element needs either stars, watchers, or forks');
+    }(), p = f[0], c = f[1], r = i[t.hasAttribute('generic-icon') ? 'generic' : p], e = null != o[a] ? o[a][p] : void 0, t.innerHTML = '<a class="gh-button-inside" href="https://github.com/' + a + '" target="_blank">\n  <div class="gh-button-svg">' + r + '</div>\n  ' + c + '\n</a>\n<a class="gh-button-count" href="https://github.com/' + a + '/' + p + '" target="_blank">\n  ' + (null != e ? e : '?') + '\n</a>', s = t.getElementsByClassName('gh-button-svg')[0].children[0], s.setAttribute('height', '16'), s.setAttribute('width', '14'), null == e) return n = t.getElementsByClassName('gh-button-count')[0], fetch(u + '://api.github.com/repos/' + a).then(function(t) {
+      if (t.ok) return t.json();
+      throw new Error('loading ' + a + ' ' + p + ' failed');
+    }).then(function(t) {
+      return o[a] = {}, o[a].stargazers = t.stargazers_count, o[a].watchers = t.watchers_count, o[a].forks = t.forks_count, n.innerHTML = o[a][p];
+    });
+  }, r = 'github-repo-button, .github-repo-button, [github-repo-button]', a = function() {
+    var t, e, n, o, i;
+    for (o = document.querySelectorAll(r), i = [], e = 0, n = o.length; e < n; e++) t = o[e], i.push(ghButtonInstall(t));
+    return i;
+  }, document.addEventListener('DOMContentLoaded', a), document.addEventListener('WebComponentsLoaded', a);
+}, function(t, e, n) {
+  var r = n(2);
+  'string' == typeof r && (r = [
+    [t.i, r, '']
+  ]);
+  var o = {
+    hmr: !0
+  };
+  o.transform = void 0;
+  n(4)(r, o);
+  r.locals && (t.exports = r.locals);
+}, function(t, e, n) {
+  e = t.exports = n(3)(void 0), e.push([t.i, '', '']);
+}, function(t, e) {
+  function n(t, e) {
+    var n = t[1] || '',
+      o = t[3];
+    if (!o) return n;
+    if (e && 'function' == typeof btoa) {
+      var i = r(o);
+      return [n].concat(o.sources.map(function(t) {
+        return '/*# sourceURL=' + o.sourceRoot + t + ' */';
+      })).concat([i]).join('\n');
+    }
+    return [n].join('\n');
+  }
+
+  function r(t) {
+    return '/*# sourceMappingURL=data:application/json;charset=utf-8;base64,' + btoa(unescape(encodeURIComponent(JSON.stringify(t)))) + ' */';
+  }
+  t.exports = function(t) {
+    var e = [];
+    return e.toString = function() {
+      return this.map(function(e) {
+        var r = n(e, t);
+        return e[2] ? '@media ' + e[2] + '{' + r + '}' : r;
+      }).join('');
+    }, e.i = function(t, n) {
+      var r = this;
+      'string' == typeof t && (t = [
+        [null, t, '']
+      ]);
+      for (var o = {}, i = 0; i < this.length; i++) {
+        var a = r[i][0];
+        'number' == typeof a && (o[a] = !0);
+      }
+      for (i = 0; i < t.length; i++) {
+        var u = t[i];
+        'number' == typeof u[0] && o[u[0]] || (n && !u[2] ? u[2] = n : n && (u[2] = '(' + u[2] + ') and (' + n + ')'), e.push(u));
+      }
+    }, e;
+  };
+}, function(t, e, n) {
+  function r(t, e) {
+    for (var n = 0; n < t.length; n++) {
+      var r = t[n],
+        o = d[r.id];
+      if (o) {
+        o.refs++;
+        for (var i = 0; i < o.parts.length; i++) o.parts[i](r.parts[i]);
+        for (; i < r.parts.length; i++) o.parts.push(p(r.parts[i], e));
+      } else {
+        for (var a = [], i = 0; i < r.parts.length; i++) a.push(p(r.parts[i], e));
+        d[r.id] = {
+          id: r.id,
+          refs: 1,
+          parts: a
+        };
+      }
+    }
+  }
+
+  function o(t, e) {
+    for (var n = [], r = {}, o = 0; o < t.length; o++) {
+      var i = t[o],
+        a = e.base ? i[0] + e.base : i[0],
+        u = i[1],
+        s = i[2],
+        c = i[3],
+        p = {
+          css: u,
+          media: s,
+          sourceMap: c
+        };
+      r[a] ? r[a].parts.push(p) : n.push(r[a] = {
+        id: a,
+        parts: [p]
+      });
+    }
+    return n;
+  }
+
+  function i(t, e) {
+    var n = g(t.insertInto);
+    if (!n) throw new Error("Couldn't find a style target. This probably means that the value for the 'insertInto' parameter is invalid.");
+    var r = w[w.length - 1];
+    if ('top' === t.insertAt) r ? r.nextSibling ? n.insertBefore(e, r.nextSibling) : n.appendChild(e) : n.insertBefore(e, n.firstChild), w.push(e);
+    else if ('bottom' === t.insertAt) n.appendChild(e);
+    else {
+      if ('object' != typeof t.insertAt || !t.insertAt.before) throw new Error("[Style Loader]\n\n Invalid value for parameter 'insertAt' ('options.insertAt') found.\n Must be 'top', 'bottom', or Object.\n (https://github.com/webpack-contrib/style-loader#insertat)\n");
+      var o = g(t.insertInto + ' ' + t.insertAt.before);
+      n.insertBefore(e, o);
+    }
+  }
+
+  function a(t) {
+    if (null === t.parentNode) return !1;
+    t.parentNode.removeChild(t);
+    var e = w.indexOf(t);
+    e >= 0 && w.splice(e, 1);
+  }
+
+  function u(t) {
+    var e = document.createElement('style');
+    return t.attrs.type = 'text/css', c(e, t.attrs), i(t, e), e;
+  }
+
+  function s(t) {
+    var e = document.createElement('link');
+    return t.attrs.type = 'text/css', t.attrs.rel = 'stylesheet', c(e, t.attrs), i(t, e), e;
+  }
+
+  function c(t, e) {
+    Object.keys(e).forEach(function(n) {
+      t.setAttribute(n, e[n]);
+    });
+  }
+
+  function p(t, e) {
+    var n, r, o, i;
+    if (e.transform && t.css) {
+      if (!(i = e.transform(t.css))) return function() {};
+      t.css = i;
+    }
+    if (e.singleton) {
+      var c = v++;
+      n = m || (m = u(e)), r = f.bind(null, n, c, !1), o = f.bind(null, n, c, !0);
+    } else t.sourceMap && 'function' == typeof URL && 'function' == typeof URL.createObjectURL && 'function' == typeof URL.revokeObjectURL && 'function' == typeof Blob && 'function' == typeof btoa ? (n = s(e), r = h.bind(null, n, e), o = function() {
+      a(n), n.href && URL.revokeObjectURL(n.href);
+    }) : (n = u(e), r = l.bind(null, n), o = function() {
+      a(n);
+    });
+    return r(t),
+    function(e) {
+      if (e) {
+        if (e.css === t.css && e.media === t.media && e.sourceMap === t.sourceMap) return;
+        r(t = e);
+      } else o();
+    };
+  }
+
+  function f(t, e, n, r) {
+    var o = n ? '' : r.css;
+    if (t.styleSheet) t.styleSheet.cssText = y(e, o);
+    else {
+      var i = document.createTextNode(o),
+        a = t.childNodes;
+      a[e] && t.removeChild(a[e]), a.length ? t.insertBefore(i, a[e]) : t.appendChild(i);
+    }
+  }
+
+  function l(t, e) {
+    var n = e.css,
+      r = e.media;
+    if (r && t.setAttribute('media', r), t.styleSheet) t.styleSheet.cssText = n;
+    else {
+      for (; t.firstChild;) t.removeChild(t.firstChild);
+      t.appendChild(document.createTextNode(n));
+    }
+  }
+
+  function h(t, e, n) {
+    var r = n.css,
+      o = n.sourceMap,
+      i = void 0 === e.convertToAbsoluteUrls && o;
+    (e.convertToAbsoluteUrls || i) && (r = x(r)), o && (r += '\n/*# sourceMappingURL=data:application/json;base64,' + btoa(unescape(encodeURIComponent(JSON.stringify(o)))) + ' */');
+    var a = new Blob([r], {
+        type: 'text/css'
+      }),
+      u = t.href;
+    t.href = URL.createObjectURL(a), u && URL.revokeObjectURL(u);
+  }
+  var d = {},
+    b = function(t) {
+      var e;
+      return function() {
+        return void 0 === e && (e = t.apply(this, arguments)), e;
+      };
+    }(function() {
+      return window && document && document.all && !window.atob;
+    }),
+    g = function(t) {
+      var e = {};
+      return function(n) {
+        if (void 0 === e[n]) {
+          var r = t.call(this, n);
+          if (r instanceof window.HTMLIFrameElement) try {
+            r = r.contentDocument.head;
+          } catch (t) {
+            r = null;
+          }
+          e[n] = r;
+        }
+        return e[n];
+      };
+    }(function(t) {
+      return document.querySelector(t);
+    }),
+    m = null,
+    v = 0,
+    w = [],
+    x = n(5);
+  t.exports = function(t, e) {
+    if ('undefined' != typeof DEBUG && DEBUG && 'object' != typeof document) throw new Error('The style-loader cannot be used in a non-browser environment');
+    e = e || {}, e.attrs = 'object' == typeof e.attrs ? e.attrs : {}, e.singleton || (e.singleton = b()), e.insertInto || (e.insertInto = 'head'), e.insertAt || (e.insertAt = 'bottom');
+    var n = o(t, e);
+    return r(n, e),
+    function(t) {
+      for (var i = [], a = 0; a < n.length; a++) {
+        var u = n[a],
+          s = d[u.id];
+        s.refs--, i.push(s);
+      }
+      if (t) {
+        r(o(t, e), e);
+      }
+      for (var a = 0; a < i.length; a++) {
+        var s = i[a];
+        if (0 === s.refs) {
+          for (var c = 0; c < s.parts.length; c++) s.parts[c]();
+          delete d[s.id];
+        }
+      }
+    };
+  };
+  var y = function() {
+    var t = [];
+    return function(e, n) {
+      return t[e] = n, t.filter(Boolean).join('\n');
+    };
+  }();
+}, function(t, e) {
+  t.exports = function(t) {
+    var e = 'undefined' != typeof window && window.location;
+    if (!e) throw new Error('fixUrls requires window.location');
+    if (!t || 'string' != typeof t) return t;
+    var n = e.protocol + '//' + e.host,
+      r = n + e.pathname.replace(/\/[^\/]*$/, '/');
+    return t.replace(/url\s*\(((?:[^)(]|\((?:[^)(]+|\([^)(]*\))*\))*)\)/gi, function(t, e) {
+      var o = e.trim().replace(/^"(.*)"$/, function(t, e) {
+        return e;
+      }).replace(/^'(.*)'$/, function(t, e) {
+        return e;
+      });
+      if (/^(#|data:|http:\/\/|https:\/\/|file:\/\/\/)/i.test(o)) return t;
+      var i;
+      return i = 0 === o.indexOf('//') ? o : 0 === o.indexOf('/') ? n + o : r + o.replace(/^\.\//, ''), 'url(' + JSON.stringify(i) + ')';
+    });
+  };
+}, function(t, e) {
+  t.exports = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 16"><path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z"></path></svg>';
+}, function(t, e) {
+  t.exports = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8.06 2C3 2 0 8 0 8s3 6 8.06 6C13 14 16 8 16 8s-3-6-7.94-6zM8 12c-2.2 0-4-1.78-4-4 0-2.2 1.8-4 4-4 2.22 0 4 1.8 4 4 0 2.22-1.78 4-4 4zm2-4c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>';
+}, function(t, e) {
+  t.exports = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 16"><path fill-rule="evenodd" d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg>';
+}, function(t, e) {
+  t.exports = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>';
+}]);
