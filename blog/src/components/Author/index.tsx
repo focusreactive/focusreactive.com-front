856fc9fe64e806fa76643e3062664ca5a9488d8f
lero62 <weblero2020@gmail.com>
2023-10-30 15:09:49 +0200

    hide authors > 3 and remove @

--- blog/src/components/Author/index.tsx
+++ blog/src/components/Author/index.tsx
@@ -6,39 +6,64 @@ import styles from "./styles.module.css";
 import { SanityApiAuthor } from "@site/src/types";
 
 type AvatarProps = {
-  avatarSize?: "small" | "big";
+	avatarSize?: "small" | "big";
 };
 
 const Avatar = styled.img<AvatarProps>`
-  ${({ avatarSize }) => (avatarSize === "big" ? "width: 80px; height 80px;" : "width: 45px; height 45px;")}
-  border-radius: 50%;
-  flex-shrink: 0;
-  aspect-ratio: 1/1;
+	${({ avatarSize }) =>
+		avatarSize === "big"
+			? "width: 80px; height 80px;"
+			: "width: 45px; height 45px;"}
+	border-radius: 50%;
+	flex-shrink: 0;
+	aspect-ratio: 1/1;
 `;
 
 type AuthorProps = AvatarProps &
-  SanityApiAuthor & {
-    isItSignature?: boolean;
-  };
+	SanityApiAuthor & {
+		isItSignature?: boolean;
+	};
 
-const Author = ({ name, id, picture, avatarSize, isItSignature, about }: AuthorProps) => (
-  <div className={styles.author}>
-    <div className={styles["author__img-wrap"]}>
-      <Avatar src={`${picture}?w=${avatarSize === "big" ? "160" : "90"}`} alt={name} avatarSize={avatarSize} />
-    </div>
-    <div className={styles.author__desc}>
-      <Link to={`/blog/author/${id}`}>
-        <p className={isItSignature ? clsx(styles.author__name, styles.author__name__signature) : styles.author__name}>
-          {name}
-        </p>
-      </Link>
-      <p
-        className={
-          isItSignature ? clsx(styles.author__company, styles.author__company__signature) : styles.author__company
-        }
-      >{`@${about}`}</p>
-    </div>
-  </div>
+const Author = ({
+	name,
+	id,
+	picture,
+	avatarSize,
+	isItSignature,
+	about,
+}: AuthorProps) => (
+	<div className={styles.author}>
+		<div className={styles["author__img-wrap"]}>
+			<Avatar
+				src={`${picture}?w=${avatarSize === "big" ? "160" : "90"}`}
+				alt={name}
+				avatarSize={avatarSize}
+			/>
+		</div>
+		<div className={styles.author__desc}>
+			<Link to={`/blog/author/${id}`}>
+				<p
+					className={
+						isItSignature
+							? clsx(styles.author__name, styles.author__name__signature)
+							: styles.author__name
+					}
+				>
+					{name}
+				</p>
+			</Link>
+			<p
+				className={
+					isItSignature
+						? clsx(
+								styles.author__company,
+								styles.author__company__signature
+						  )
+						: styles.author__company
+				}
+			>{`${about}`}</p>
+		</div>
+	</div>
 );
 
 export default Author;
