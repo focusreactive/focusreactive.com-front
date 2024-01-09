06a7b52ca395805ab6d15bf68e6fa9a838faf141
Petro <83351038+lero62@users.noreply.github.com>
2024-01-09 10:17:09 +0200

    add section faq (#16)

--- src/js/components/accordion.js
+++ src/js/components/accordion.js
@@ -17,4 +17,18 @@ export function accordion() {
       $(this).find('.clients-cases__right').slideDown();
     }
   });
+
+  $('[data-spoller]').on('click', function () {
+    var el = $(this);
+    $(this).closest('[data-spollers]').find('[data-spoller]').not($(this)).removeClass('_active');
+    $(this)
+      .closest('[data-spollers]')
+      .find('[data-spoller]')
+      .next()
+      .not($(this).next())
+      .slideUp(200);
+    el.next().slideToggle(200);
+    el.toggleClass('_active');
+    return false;
+  });
 }
