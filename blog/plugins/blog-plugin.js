a70f3a90beec76c6b1c146bcc310c418e2d01c03
Sergey Labut <sergeylabut@gmail.com>
2024-04-19 16:43:33 +0200

    Delete hotjar

--- blog/plugins/blog-plugin.js
+++ blog/plugins/blog-plugin.js
@@ -401,20 +401,6 @@ async function blogPluginExtended(...pluginArgs) {
             async: true,
             tagName: 'script',
             innerHTML: `
-               (function (h, o, t, j, a, r) {
-                h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
-                h._hjSettings = { hjid: 3443963, hjsv: 6 };
-                a = o.getElementsByTagName('head')[0];
-                r = o.createElement('script'); r.defer = 1;
-                r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-                a.appendChild(r);
-              })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-             `,
-          },
-          {
-            async: true,
-            tagName: 'script',
-            innerHTML: `
                var _gauges = _gauges || [];
                   (function () {
                     var t = document.createElement('script');
@@ -441,8 +427,9 @@ async function blogPluginExtended(...pluginArgs) {
         postBodyTags: [footerHtml],
       };
     },
-    postBuild: function () // Do we need this?
-    {},
+    postBuild: function () {
+      // Do we need this?
+    },
   };
 }
 
